5aa1af6925b5f617f64b6ac57e945b7b17bf2316
nicopetelo <nicopetelo@gmail.com>
2021-04-11 23:29:30 -0700

    trying out push

--- js/cleanup.js
+++ js/cleanup.js
@@ -22,7 +22,6 @@ function passParameters() {
   needLowerChar = confirm("Do you want lowercase letters?");
   needSpeChar = confirm("Do you want special characters?");
   needNumber = confirm("Do you want numbers?");
-
 }
 
 generateBtn.addEventListener("click", passParameters);
@@ -32,170 +31,37 @@ generateBtn.addEventListener("click", passParameters);
 //    return Math.floor(Math.random() * 4);
 //}
 
-
 function generatePassword() {
-    // all characters allowed
-    if (needUpperChar === true && needLowerChar === true 
-        && needNumber === true && needSpeChar === true) {
-            var typesOfChar = [lowerChar,upperChar,speChar,number];
-            function randomTypeOfChar() {
-              return Math.floor(Math.random() * 4);
-            }
-            for (i = 0; i < howManyCharNeeded; i++) {
-                typeNum = randomTypeOfChar();
-                type = typesOfChar[typeNum];
-                randomChar = type.charAt(Math.floor(Math.random() * type.length) );
-                passPiece = randomChar.toString();
-                password += passPiece;
-            }
-            return password;
-        }
-    
-    // no numbers
-    if (needUpperChar === true && needLowerChar === true 
-      && needNumber === false && needSpeChar === true) {
-          var typesOfChar = [lowerChar,upperChar,speChar];
-          function randomTypeOfChar() {
-            return Math.floor(Math.random() * 3);
-          }
-          for (i = 0; i < howManyCharNeeded; i++) {
-              typeNum = randomTypeOfChar();
-              type = typesOfChar[typeNum];
-              randomChar = type.charAt(Math.floor(Math.random() * type.length) );
-              passPiece = randomChar.toString();
-              password += passPiece;
-          }
-          return password;
-      }
-    // no speChar
-    if (needUpperChar === true && needLowerChar === true 
-      && needNumber === true && needSpeChar === false) {
-          var typesOfChar = [lowerChar,upperChar,number];
-          function randomTypeOfChar() {
-            return Math.floor(Math.random() * 3);
-          }
-          for (i = 0; i < howManyCharNeeded; i++) {
-              typeNum = randomTypeOfChar();
-              type = typesOfChar[typeNum];
-              randomChar = type.charAt(Math.floor(Math.random() * type.length) );
-              passPiece = randomChar.toString();
-              password += passPiece;
-          }
-          return password;
-        }
-    // no speChar or numbers
-    if (needUpperChar === true && needLowerChar === true 
-      && needNumber === false && needSpeChar === false) {
-          var typesOfChar = [lowerChar,upperChar];
-          function randomTypeOfChar() {
-            return Math.floor(Math.random() * 2);
-          }
-          for (i = 0; i < howManyCharNeeded; i++) {
-              typeNum = randomTypeOfChar();
-              type = typesOfChar[typeNum];
-              randomChar = type.charAt(Math.floor(Math.random() * type.length) );
-              passPiece = randomChar.toString();
-              password += passPiece;
-          }
-          return password;
-      }
-    // no speChar or numbers or uppercase
-    if (needUpperChar === false && needLowerChar === true 
-      && needNumber === false && needSpeChar === false) {
-          var typesOfChar = [lowerChar];
-          function randomTypeOfChar() {
-            return Math.floor(Math.random() * 1);
-          }
-          for (i = 0; i < howManyCharNeeded; i++) {
-              typeNum = randomTypeOfChar();
-              type = typesOfChar[typeNum];
-              randomChar = type.charAt(Math.floor(Math.random() * type.length) );
-              passPiece = randomChar.toString();
-              password += passPiece;
-          }
-          return password;
-      }
-    // no speChar or numbers or lowercase
-    if (needUpperChar === true && needLowerChar === false 
-      && needNumber === false && needSpeChar === false) {
-          var typesOfChar = [upperChar];
-          function randomTypeOfChar() {
-            return Math.floor(Math.random() * 1);
-          }
-          for (i = 0; i < howManyCharNeeded; i++) {
-              typeNum = randomTypeOfChar();
-              type = typesOfChar[typeNum];
-              randomChar = type.charAt(Math.floor(Math.random() * type.length) );
-              passPiece = randomChar.toString();
-              password += passPiece;
-          }
-          return password;
-      }
-    // no uppercase or numbers or lowercase
-    if (needUpperChar === false && needLowerChar === false 
-      && needNumber === false && needSpeChar === true) {
-          var typesOfChar = [speChar];
-          function randomTypeOfChar() {
-            return Math.floor(Math.random() * 1);
-          }
-          for (i = 0; i < howManyCharNeeded; i++) {
-              typeNum = randomTypeOfChar();
-              type = typesOfChar[typeNum];
-              randomChar = type.charAt(Math.floor(Math.random() * type.length) );
-              passPiece = randomChar.toString();
-              password += passPiece;
-          }
-          return password;
-      }
-    // no uppercase or speChar or lowercase
-    if (needUpperChar === false && needLowerChar === false 
-      && needNumber === true && needSpeChar === false) {
-          var typesOfChar = [number];
-          function randomTypeOfChar() {
-            return Math.floor(Math.random() * 1);
-          }
-          for (i = 0; i < howManyCharNeeded; i++) {
-              typeNum = randomTypeOfChar();
-              type = typesOfChar[typeNum];
-              randomChar = type.charAt(Math.floor(Math.random() * type.length) );
-              passPiece = randomChar.toString();
-              password += passPiece;
-          }
-          return password;
-      }
-    // no lowercase
-    if (needUpperChar === true && needLowerChar === false 
-      && needNumber === true && needSpeChar === true) {
-          var typesOfChar = [upperChar, speChar, number];
-          function randomTypeOfChar() {
-            return Math.floor(Math.random() * 3);
-          }
-          for (i = 0; i < howManyCharNeeded; i++) {
-              typeNum = randomTypeOfChar();
-              type = typesOfChar[typeNum];
-              randomChar = type.charAt(Math.floor(Math.random() * type.length) );
-              passPiece = randomChar.toString();
-              password += passPiece;
-          }
-          return password;
-      }
-    // no upper
-    if (needUpperChar === false && needLowerChar === true 
-      && needNumber === true && needSpeChar === true) {
-          var typesOfChar = [lowerChar, speChar, number];
-          function randomTypeOfChar() {
-            return Math.floor(Math.random() * 3);
-          }
-          for (i = 0; i < howManyCharNeeded; i++) {
-              typeNum = randomTypeOfChar();
-              type = typesOfChar[typeNum];
-              randomChar = type.charAt(Math.floor(Math.random() * type.length) );
-              passPiece = randomChar.toString();
-              password += passPiece;
-          }
-          return password;
-      }
+	var typesOfChar = [];
+	var numOfTypes = 0;
+	if (needUpperChar === true) {
+		typesOfChar.push(upperChar);
+		numOfTypes++;
+	}
+	if (needLowerChar === true) {
+		typesOfChar.push(lowerChar);
+		numOfTypes++;
+	}
+	if (needSpeChar === true) {
+		typesOfChar.push(speChar);
+		numOfTypes++;
+	}
+	if (needNumber === true) {
+		typesOfChar.push(number)
+		numOfTypes++;
+	}
+	var password = "";
+	for (i = 0; i < howManyCharNeeded; i++) {
+		var typeNum = Math.floor(Math.random() * numOfTypes);
+		var type = typesOfChar[typeNum];
+		var randomChar = type.charAt(Math.floor(Math.random() * type.length) );
+    var passPiece = randomChar.toString();
+    password += passPiece;
+	}
+	return password;
 }
+
+
 
 // Write password to the #password input
 function writePassword() {
